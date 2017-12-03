4622310991b7efc2b80065a637393abde43a6cd8
liusitan <liust9200@live.com>
2017-12-03 05:14:25 -0800

    Update Data_cosmetic_surgery.py

--- Data_cosmetic_surgery.py
+++ Data_cosmetic_surgery.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from scipy import signal
+# virtual dataset
 np.random.seed(2017)
 Spiking_Record = np.random.randint(0,high=2,size=(64,1000))
 plt.plot(Spiking_Record[1], label='channel'+str(1))
-def Cconversion_Continuous_Gaussian(W_gaussian,std_gaussian)#,dt)??
+#
+# chosen_array is to sepcify a certain group of the signal data to make the pca predictor more flexible
+#(I don't if it will be useful, it's just an option')
+def Cconversion_Continuous_Gaussian(Spiking_Record, W_gaussian,std_gaussian,chosen_array=None):
+#,dt)??
+    if chosen_array !=None:
+        Spiking_Record = Spiking_Record[chose_array,:]
     Gaussian_window = signal.gaussian(M=100,std=4)#,sym=False)
     plt.plot(Gaussian_window)
     for i,value in enumerate(Spiking_Record):
@@ -16,6 +23,5 @@ def Cconversion_Continuous_Gaussian(W_gaussian,std_gaussian)#,dt)??
     plt.legend(loc='best')
     Spiking_Record=np.transpose(Spiking_Record)
     PCA_result=decomposition.PCA(n_components=3,whiten=True)
-    pcs = PCA_result.fit(Spiking_Record).transform(Spiking_Record)
-    #pcs = np.transpose(pcs)
-    plt.plot(pcs)
+    pcs = PCA_result.fit(Spiking_Record).transform(Spiking_Record) #pcs is the collection of all the chose pca
+    return pcs
